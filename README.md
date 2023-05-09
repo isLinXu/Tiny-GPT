e7baaabade4edb88dce2ea08d4615b4d1bdb63a2
isLinXu <2267379130@qq.com>
2023-05-09 22:59:17 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Tiny-GPT
+# Quora Poe reverse-engineered API
+This is a reverse-engineered API for Quora's Poe that allows access to the following chatbots:
+
+1. Sage - OpenAI (capybara)
+2. GPT-4 - OpenAI (beaver)
+3. Claude+ - Anthropic (a2_2)
+4. Claude - Anthropic (a2)
+5. ChatGPT - OpenAI (chinchilla)
+6. Dragonfly - OpenAI (nutria)
+### Requirements
+To use this API, you will need to have the following cookies:
+
+2. Quora-Formkey: This is obtained by logging in to Quora.com, viewing the page source, and finding the "formkey" dictionary key. Use its value in the Quora-Formkey field.
+3. Cookie: 'm-b=xxxx' - This is the value of the cookie with the key m-b, which is present in the list of cookies used on Quora.com, you can simply inspect cookies in Chrome to get it.
+
+### Example
+You can find an example of how to use this API in the example.py file.
+
+### Disclaimer
+This repository is for educational and research purposes only, and the use of this API for any other purpose is at your own risk. We are not responsible for any actions taken by users of this API.
+
+#### Requirements: 
+requests. 
---
+++ example.py
@@ -0,0 +1,33 @@
+from POE import load_chat_id_map, clear_context, send_message, get_latest_message, set_auth
+
+#Auth
+set_auth('Quora-Formkey','0fda7ce8e56d00853ce5f62d0076353b')
+set_auth('Cookie','m-b=QGmL0_XlTQQg662Oq3dXYg')
+#---------------------------------------------------------------------------
+print("Who do you want to talk to?")
+print("1. Sage - OpenAI (capybara)")
+print("2. GPT-4 - OpenAI (beaver)")
+print("3. Claude+ - Anthropic (a2_2)")
+print("4. Claude - Anthropic (a2)")
+print("5. ChatGPT - OpenAI (chinchilla)")
+print("6. Dragonfly - OpenAI (nutria)")
+#---------------------------------------------------------------------------
+option = input("Please enter your choice : ")
+bots = {1:'capybara', 2:'beaver', 3:'a2_2', 4:'a2', 5:'chinchilla', 6:'nutria'}
+bot = bots[int(option)]
+print("The selected bot is : ", bot)
+#---------------------------------------------------------------------------
+chat_id = load_chat_id_map(bot)
+clear_context(chat_id)
+print("Context is now cleared")
+while True:
+    message = input("Human : ")
+    if message =="!clear":
+        clear_context(chat_id)
+        print("Context is now cleared")
+        continue
+    if message =="!break":
+        break
+    send_message(message,bot,chat_id)
+    reply = get_latest_message(bot)
+    print(f"{bot} : {reply}")
